9fccf11dc40c3ceda212ecf5c91cc9ffda06391f
roguione <myjobcontacts@yahoo.com>
2024-04-09 00:20:34 -0500

    mostly setup, screwed up mongodb

---
+++ server.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+require('./config/database');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const app = express();
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const PORT = process.env.PORT || 3001; 
+const mongoURI = process.env.MONGODB_URI;
+
+// Connect to MongoDB
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('MongoDB connected');
+})
+.catch(err => console.error(err));
+
+// Middleware
+app.use(express.json()); 
+app.use(logger('dev'));
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Routes
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
