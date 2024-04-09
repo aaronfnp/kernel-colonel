a14e16997d1531000faa5452158f58128898d7f8
roguione <myjobcontacts@yahoo.com>
2024-04-09 09:12:41 -0500

    build out src file structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import App from './pages/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,23 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { getUser } from '../../utilities/users-service';
+import AuthPage from '../AuthPage/AuthPage';
+import NavBar from '../../components/NavBar/NavBar';
 import './App.css';
 
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    // Fetch data from Express backend
-    fetch('/api/message')
-      .then(response => response.json())
-      .then(data => setMessage(data.message))
-      .catch(error => console.error(error));
-  }, []);
+  const [user, setUser] = useState(getUser());
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>{message}</p>
-      </header>
-    </div>
+    <main className="App">
+      { user ?
+        <>
+          <NavBar user={user} setUser={setUser} />
+          <Routes>
+            {/* Route components in here */}
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders" element={<OrderHistoryPage />} />
+          </Routes>
+        </>
+        :
+        <AuthPage setUser={setUser} />
+      }
+    </main>
   );
 }
 
